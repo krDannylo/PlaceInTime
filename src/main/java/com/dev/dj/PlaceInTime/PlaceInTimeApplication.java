a84fc2ad12218fa05cd8740c405407fd4cc2ff0f
Joao Oliveira <joaooliveira@Joaos-MacBook-Air.local>
2025-10-27 23:44:05 -0300

    start

---
+++ src/main/java/com/dev/dj/PlaceInTime/PlaceInTimeApplication.java
@@ -0,0 +1,14 @@
+package com.dev.dj.PlaceInTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceInTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceInTimeApplication.class, args);
+        IO.print("Ola mundo!!!");
+	}
+
+}
